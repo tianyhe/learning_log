0e68109daa1cbb9a28306b00fb3cf541e1c7387d
tianyhe <tianyao.he@outlook.com>
2020-05-01 17:05:37 +0800

    Added 'Add Topic, Add Entry & Edit Entry functions'

---
+++ learning_logs/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+
+from .models import Topic, Entry
+
+class TopicForm(forms.ModelForm):
+    """A class to define the topic form."""
+    class Meta:
+        model = Topic
+        fields = ['text']
+        labels = {'text': ''}
+
+class EntryForm(forms.ModelForm):
+    """A class to define the entry form."""
+    class Meta:
+        model = Entry
+        fields = ['text']
+        labels = {'text': ''}
+        widgets = {'text': forms.Textarea(attrs={'cols': 80})}
